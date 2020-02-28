e4b4c96d4ca3b5ac0a88375340a4e4abd1cbcc8c
HyeonJu An <ahj9680@daum.net>
2020-02-29 08:57:46 +0900

    python basic

--- chapter08_01.py
+++ chapter08_01.py
@@ -6,13 +6,6 @@
 
 # 절대값
 # abs()
-import sys
-
-import io
-
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-
-sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 print(abs(-3))
 
