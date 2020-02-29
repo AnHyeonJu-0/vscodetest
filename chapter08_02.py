f433b8abda4019ff85545cd435aec5488b1e29db
HyeonJu An <ahj9680@daum.net>
2020-02-29 09:46:36 +0900

    second commit

--- chapter08_02.py
+++ chapter08_02.py
@@ -1,5 +1,5 @@
 #Chapter08-2
-#파이썬 외장(External)함수
+# 파이썬 외장(External)함수
 # 실제 프로그램 개발 중 자주 사용
 # 종류 : sys, pickle, shutil, temfile, time, random 등
 
@@ -10,7 +10,7 @@ print(sys.argv)
 #예제2(강제 종료)
 #sys.exit()
 
-# 예쩨3(파이썬 패키지 위치)
+# 예제3(파이썬 패키지 위치)
 print(sys.path)
 
 # pickle : 객체 파일 쓰기
@@ -88,6 +88,5 @@ import webbrowser
 webbrowser.open("http://google.com")
 
 webbrowser.open_new("http://google.com")
-
 
 
